60b16e136c00f5c8a2da6e1b06c5dcf76691c6c9
ngocgiauvo <ngocgiau18tqdn@gmail.com>
2018-04-19 02:08:25 +0700

    Tạo các màn hình còn lại (chưa có nội dung), navigation

---
+++ GUI/UIReceptionist/MakeListPatient.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using QLPhongKhamTuNhan.GUI.UIReceptionist;
+
+namespace QLPhongKhamTuNhan.GUI.UIReceptionist
+{
+    /// <summary>
+    /// Interaction logic for MakeListPatient.xaml
+    /// </summary>
+    public partial class MakeListPatient : Page
+    {
+        public MakeListPatient()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            CreateBill bill = new CreateBill();
+            bill.ShowDialog();
+        }
+    }
+}
