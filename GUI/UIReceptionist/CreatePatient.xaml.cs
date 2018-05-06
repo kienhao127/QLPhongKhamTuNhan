3c893b6da4b36b078fe7016b14c83f00d2fcbcca
kienhao127 <luongkienhao@gmail.com>
2018-05-06 20:45:05 +0700

    update code chỉ nhập số ở txt Ngày Sinh

--- GUI/UIReceptionist/CreatePatient.xaml.cs
+++ GUI/UIReceptionist/CreatePatient.xaml.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -81,6 +82,12 @@ namespace QLPhongKhamTuNhan.GUI.UIReceptionist
             
 
             Close();
+        }
+
+        private void NumberValidationTextBox(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[^0-9]+");
+            e.Handled = regex.IsMatch(e.Text);
         }
     }
 }
