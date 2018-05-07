5bec4b2f7ef6165de91a355eba4ff31677dfabb7
Yount Zitxh <ttngoc653@gmail.com>
2018-05-07 17:22:19 +0700

    sua loi cau truy van + them tinh nang tinh tong doanh thu va so luot kham trong thang thuoc bao cao doanh thu trong thang theo ngay

--- Manager/DataReport.cs
+++ Manager/DataReport.cs
@@ -13,22 +13,32 @@ namespace QLPhongKhamTuNhan.Manager
     {
         static public DataTable MonthlyRevenue(string month,string year)
         {            
+            MessageBox.Show("SELECT DATE(m0.date_exam) date_exam, " +
+                                 "COUNT(*) num_patient, " +
+                                 "SUM(m0.fee_exam + m0.fee_medicine) day_revenue, " +
+                                 "sum(m0.fee_exam + m0.fee_medicine) / totals.total radio " +
+                                 "FROM medical_exam m0, (" +
+                                 "    select SUM(m1.fee_exam + m1.fee_medicine) total " +
+                                 "    from medical_exam m1" +
+                                 "    WHERE MONTH(m1.date_exam) = " + month +
+                                 "        and YEAR(m1.date_exam) = " + year +
+                                 ") as totals " +
+                                 "WHERE MONTH(m0.date_exam) = " + month +
+                                 "        and YEAR(m0.date_exam) = " + year +
+                                 " GROUP by DATE(m0.date_exam);");
             return Active.select("SELECT DATE(m0.date_exam) date_exam, " +
                                  "COUNT(*) num_patient, " +
                                  "SUM(m0.fee_exam + m0.fee_medicine) day_revenue, " +
                                  "sum(m0.fee_exam + m0.fee_medicine) / totals.total radio " +
                                  "FROM medical_exam m0, (" +
-                                 "    select date(m1.date_exam) date_exam, " +
-                                 "    SUM(m1.fee_exam + m1.fee_medicine) total " +
+                                 "    select SUM(m1.fee_exam + m1.fee_medicine) total " +
                                  "    from medical_exam m1"  +
                                  "    WHERE MONTH(m1.date_exam) = " + month +
                                  "        and YEAR(m1.date_exam) = " + year +
-                                 "    GROUP BY date(m1.date_exam)" +
                                  ") as totals " +
                                  "WHERE MONTH(m0.date_exam) = " + month +
                                  "        and YEAR(m0.date_exam) = " + year +
-                                 "        and DATE(totals.date_exam) = DATE(m0.date_exam) " +
-                                 "GROUP by DATE(m0.date_exam);");
+                                 " GROUP by DATE(m0.date_exam);");
         }
 
         static public DataTable UseMedicine(string month, string year)
